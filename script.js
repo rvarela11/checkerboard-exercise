a850a3ee6f40a4e6a4b2db1d5a4e120a4f1166c8
Roberto  Varela <rvarela11@Roberts-MacBook-Pro.local>
2016-08-26 10:56:01 -0500

    Random Colors Complete

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+var b = document.getElementsByTagName("body");
+var body = b[0];
+
+function makeGrid() {
+  for (i = 1; i < 55; i++) {
+    var box = document.createElement("div");
+    box.style.width = "11.1%";
+    box.style.float = "left";
+    box.style.paddingBottom = "11.1%";
+    var r = Math.floor(Math.random() * 250);
+    var g = Math.floor(Math.random() * 250);
+    var b = Math.floor(Math.random() * 250);
+    box.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    body.appendChild(box);
+  }
+}
+makeGrid();
