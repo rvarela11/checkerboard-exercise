197231da6a7b8fc6ee0b0d1a5613d7ca0dfc0157
Roberto  Varela <rvarela11@Roberts-MacBook-Pro.local>
2016-08-26 11:37:26 -0500

    Flash with audtio complete

--- script.js
+++ script.js
@@ -7,12 +7,31 @@ function makeGrid() {
     box.style.width = "11.1%";
     box.style.float = "left";
     box.style.paddingBottom = "11.1%";
-    if (i % 2 === 0) {
-      box.style.backgroundColor = "rgba(173, 33, 99, " + (0.02 * i) + ")";
-    } else {
-      box.style.backgroundColor = "rgba(13, 133, 199, " + (0.02 * i) + ")";
-    }
+    var r = Math.floor(Math.random() * 250);
+    var g = Math.floor(Math.random() * 250);
+    var b = Math.floor(Math.random() * 250);
+    box.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
     body.appendChild(box);
   }
 }
 makeGrid();
+
+function color() {
+  var boxesColors = document.getElementsByTagName("div");
+  for (i = 0; i < boxesColors.length; i++) {
+    var r = Math.floor(Math.random() * 250);
+    var g = Math.floor(Math.random() * 250);
+    var b = Math.floor(Math.random() * 250);
+    boxesColors[i].style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+  }
+}
+
+setInterval(function() {
+  var boxes = document.getElementsByTagName("div");
+  for (i = 0; i < boxes.length; i++) {
+    boxes[i].style.backgroundColor = color();
+  }
+}, 2000);
+
+var audio = new Audio('discoBalls.mp3');
+audio.play();
