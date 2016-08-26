6bb4cd1f5d864001e07a6bc0595ad81149dcf599
Roberto  Varela <rvarela11@Roberts-MacBook-Pro.local>
2016-08-26 10:35:53 -0500

    Normal Checkerborad Complete

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Your JS goes here
+var b = document.getElementsByTagName("body");
+var body = b[0];
+
+function makeGrid() {
+  for (i = 1; i < 55; i++) {
+    var box = document.createElement("div");
+    box.style.width = "11.1%";
+    box.style.float = "left";
+    box.style.paddingBottom = "11.1%";
+    if (i % 2 === 0) {
+      box.style.background = "red";
+    } else {
+      box.style.background = "#000";
+    }
+    body.appendChild(box);
+  }
+}
+makeGrid();
+
+console.log(body);
